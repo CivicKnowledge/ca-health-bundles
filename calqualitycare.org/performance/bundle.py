9286c8798abb8a79ea6051d95c447e299de37f7e
Eric Busboom <eric@clarinova.com>
2015-05-24 09:48:04 -0700

    added bundle

---
+++ calqualitycare.org/performance/bundle.py
@@ -0,0 +1,14 @@
+""""""
+
+from ambry.bundle.loader import ExcelBuildBundle
+
+
+class Bundle(ExcelBuildBundle):
+
+    """"""
+
+    def __init__(self, directory=None):
+
+        super(Bundle, self).__init__(directory)
+
+
