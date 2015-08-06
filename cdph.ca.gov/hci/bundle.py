209bccba82465c19261095cd97e06fe18a7ea9ae
Eric Busboom <eric@clarinova.com>
2015-08-06 07:44:28 -0700

    .

--- cdph.ca.gov/hci/bundle.py
+++ cdph.ca.gov/hci/bundle.py
@@ -1,21 +1,10 @@
 """"""
 
-from ambry.bundle import BuildBundle
+from ambry.bundle.loader import ExcelBuildBundle
 
 
-class Bundle(BuildBundle):
+class Bundle(ExcelBuildBundle):
 
     """"""
 
-    def __init__(self, directory=None):
-
-        super(Bundle, self).__init__(directory)
-
-    def download(self):
-        
-        for source in self.sources():
-            print source
-
-    def build(self):
-
-        return True
+    pass
