b7cdbcd36e59bb9db3f4ba5c3f974ac327f8cc72
MiZHGUN <mizhgun@gmail.com>
2015-08-10 22:58:21 +0300

    Add gvid (#322) and validate sources for: food_affordability healthy_food high_school_ed income_inequality open_space unemployment

--- cdph.ca.gov/hci/bundle.py
+++ cdph.ca.gov/hci/bundle.py
@@ -1,10 +1,26 @@
 """"""
 
 from ambry.bundle.loader import ExcelBuildBundle
+from ambry.util import memoize
 
 
 class Bundle(ExcelBuildBundle):
 
     """"""
 
-    pass
+    @property
+    @memoize
+    def counties_map(self):
+        counties = { r['name'].replace(" County, California",'').lower(): r['gvid']
+                     for r in  self.library.dep('counties').partition.rows  if int(r['state'] == 6)}
+
+        return counties
+
+
+    def build_modify_row(self, row_gen, p, source, row):
+
+
+            try:
+                row['gvid'] =  self.counties_map[row['county_name'].lower()]
+            except KeyError:
+                pass
